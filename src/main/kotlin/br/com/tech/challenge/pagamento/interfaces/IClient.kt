748c95ac515358e842990f850a357654847b7a61
carla-gb <carla.paula@grupoboticario.com.br>
2024-11-20 17:48:43 -0300

    ajusta client de pagamento

--- src/main/kotlin/br/com/tech/challenge/pagamento/interfaces/IClient.kt
+++ src/main/kotlin/br/com/tech/challenge/pagamento/interfaces/IClient.kt
@@ -4,8 +4,8 @@ import br.com.tech.challenge.pagamento.core.entity.Payment
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.*
 
-@FeignClient(url = "svc-loja", value = "loja")
-//@FeignClient(url = "localhost:8080", value = "loja")
+@FeignClient(url = "svc-pedido", value = "pedido")
+//@FeignClient(url = "localhost:8082", value = "pedido")
 interface IClient {
 
   @RequestMapping(method = [RequestMethod.POST], value = ["/pedido/pagamento"])
